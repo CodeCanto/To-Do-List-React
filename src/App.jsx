b52c93e76c80fda83d616595494fd3e9aee1b112
Entelodonto <jamesdanielporter@proton.me>
2023-06-10 21:44:37 +0100

    prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const taskData = [];
+
+  const [taskList, setTaskList] = useState(taskData);
+  const [taskDescription, setTaskDescription] = useState("");
+  const [dateComplete, setDateComplete] = useState("");
+  const [datePosted, setDatePosted] = useState("");
+
+  const submitHandler = (event) => {
+    console.log("Test");
+    event.preventDefault();
+    let task = {
+      taskDescription: taskDescription,
+      taskComplete: dateComplete,
+      taskPosted: datePosted,
+    };
+
+    setTaskList((prevTaskList) => {
+      return [...prevTaskList, task];
+    });
+
+    setTaskDescription("");
+  };
+
+  const handleTaskName = (e) => {
+    setTaskDescription(e.target.value);
+  };
+
+  const handleDateComplete = (e) => {
+    setDateComplete(e.target.value);
+  };
+
+  const handleDatePosted = (e) => {
+    setDatePosted(e.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Task List</h1>
+      <form onSubmit={submitHandler}>
+        <label>
+          Goal To Do
+          <input
+            type="text"
+            onChange={handleTaskName}
+            value={taskDescription}
+          ></input>
+        </label>
+        <label>
+          Date to complete by
+          <input type="date" onChange={handleDateComplete}></input>
+        </label>
+        <label>
+          Date Added
+          <input type="date" onChange={handleDatePosted}></input>
+        </label>
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {taskList.map((task) => {
+          return (
+            <>
+              <p>{task.taskDescription}</p>
+              <p>{task.tastComplete}</p>
+              <p>{task.taskPosted}</p>
+            </>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
